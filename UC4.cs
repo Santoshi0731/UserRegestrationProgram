0adbad1ad9f78764b7a11ef498b2cae8682637bf
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-02-19 17:55:18 +0530

    Add files via upload

---
+++ UC4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class UserName3
+    {
+        public static void PhoneNumberValidating()      //UC4
+        {
+            Console.WriteLine("Enter your PhoneNumber");
+            string userInput = Console.ReadLine();
+            string regexCondition = "^(+)[0-9]{2}[ ][0-9]{10}$";
+            Iteration(userInput, regexCondition);
+        }
+
+        public static void Iteration(string userInput, string regexCondition)
+        {
+            if (Regex.IsMatch(userInput, regexCondition))
+            {
+                Console.WriteLine("Validated successfully!\n");
+            }
+            else
+            {
+                Console.WriteLine("Entered Details are not in required format.Please try again!\n");
+            }
+        }
+    }
+}
