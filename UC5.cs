156850dcd37c94413dbc03bff842eec6b474c189
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-02-19 17:56:24 +0530

    Add files via upload

---
+++ UC5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class UserName4
+    {
+        public static void Passmin8char()      //UC5
+        {
+            Console.WriteLine("Enter your Password");
+            string userInput = Console.ReadLine();
+            string regexCondition = "^[a-zA-Z0-9]{8,}$";
+            Iteration(userInput, regexCondition);
+        }
+
+        public static void Iteration(string userInput, string regexCondition)
+        {
+            if (Regex.IsMatch(userInput, regexCondition))
+            {
+                Console.WriteLine("Validated successfully!\n");
+            }
+            else
+            {
+                Console.WriteLine("Entered Details are not in required format.Please try again!\n");
+            }
+        }
+    }
+}
